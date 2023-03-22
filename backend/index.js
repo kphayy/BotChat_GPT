035bf0ce3b7a671ea9700c7a374f664a485c2e37
ThanhDat20mr <crystalblued@gmail.com>
2023-03-22 16:06:48 +0700

    sua index

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-    return res.status(200).send("hello word");
+    return res.status(200).send("hello world 2222222s");
 })
 
 app.listen(8000, () => {
